434f3c8846fa40cf2d7f378b70e86d87c79fccbb
Siphe23 <siphesihlemzamane@gmai.com>
2025-02-09 22:14:06 +0200

    Updated files

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,27 +1,27 @@
-import JWT from 'jsonwebtoken'
-import userModel from '../models/userModel.js'
+import JWT from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
 export const isAuth = async (req, res, next) => {
-    const { token } = req.cookies;
-
-    // Validation: check if token exists
-    if (!token) {
-        return res.status(401).send({
-            success: false,
-            message: 'Unauthorized user',
-        });
-    }
-
-    try {
-        const decodeData = JWT.verify(token, process.env.JWT_SECRET);
-        req.user = await userModel.findById(decodeData._id); // Attach user info to request
-        next(); // Proceed to next middleware or route handler
-    } catch (error) {
-        return res.status(401).send({
-            success: false,
-            message: 'Invalid or expired token',
-        });
-    }
+  // Try to get token from cookies; if not present, get it from the Authorization header.
+  const token = req.cookies.token || (req.headers.authorization && req.headers.authorization.split(" ")[1]);
+  
+  if (!token) {
+    return res.status(401).send({
+      success: false,
+      message: "Unauthorized user",
+    });
+  }
+  
+  try {
+    const decodeData = JWT.verify(token, process.env.JWT_SECRET);
+    req.user = await userModel.findById(decodeData._id);
+    next();
+  } catch (error) {
+    return res.status(401).send({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
 };
 
 
